feb0c4dac35e2fd8397683d1791588680c983f8e
Memme_ca81 <Memme_ca81@STHQ0115-10>
2024-04-27 14:06:59 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+    
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Car>? Cars { get; set; }
+        public MainWindow()
+        {
+            Cars = new ObservableCollection<Car>();
+            Cars.Add(new Car() { Model = "Nissan", Vendor = "KIa", Year = "1222" });
+            Cars.Add(new Car() { Model = "Hunday", Vendor = "KIa", Year = "1222" });
+            Cars.Add(new Car() { Model = "BMW", Vendor = "KIa", Year = "1222" });
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ListView_PreviewMouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Update_AddWindow update_AddWindow = new Update_AddWindow();
+            update_AddWindow.Car = (sender as ListView).SelectedItem as Car;
+            update_AddWindow.Text = "Update";
+            update_AddWindow.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/UpdateWindows.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+  
+    public partial class UpdateWindows : Window
+    {
+        private Car? car;
+        private string? text;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string? Text { get => text; set { text = value; OnPropertyChanged(); } }
+        public Car? Car { get => car; set { car = value; OnPropertyChanged(); } }
+        public UpdateWindows()
+        {
+            InitializeComponent();
+        }
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAddUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
